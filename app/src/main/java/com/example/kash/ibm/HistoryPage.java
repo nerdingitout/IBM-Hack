0e06343b7c3819966037b0d068dd3f73c10a23b6
Nora-Ba-Fadhl <norahbafadhl@gmail.com>
2019-03-24 00:15:54 +0400

    Interview page with ChatBot Added. Presentation page Added. History page Added.

---
+++ app/src/main/java/com/example/kash/ibm/HistoryPage.java
@@ -0,0 +1,51 @@
+package com.example.kash.ibm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HistoryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.homeItem:
+
+                Intent intent;
+                intent = new Intent(this, home.class);
+
+                return true;
+
+            case R.id.settingsItem:
+
+                Intent intent2;
+                intent2 = new Intent(this, home.class);
+
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/kash/ibm/home.java
+++ app/src/main/java/com/example/kash/ibm/home.java
@@ -1,15 +1,70 @@
 package com.example.kash.ibm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class home extends AppCompatActivity {
+
+    private static String TAG = "MAIN";
+    private Button button2;
+    private Button button3;
+    private Button button5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Log.d(TAG, "onCreate()");
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button5 = findViewById(R.id.button5);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openInterviewPage();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPresentationPage();
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHistoryPage();
+            }
+        });
+
+
 
     }
+    public void openInterviewPage()
+    {
+        Intent intent;
+        intent = new Intent(this, InterviewPractice.class);
+    }
+    public void openPresentationPage()
+    {
+
+        Intent intent;
+        intent = new Intent(this, PresentationPractice.class);
+
+    }
+    public void openHistoryPage()
+    {
+        Intent intent;
+        intent = new Intent(this, HistoryPage.class);
+    }
+
+
 }
